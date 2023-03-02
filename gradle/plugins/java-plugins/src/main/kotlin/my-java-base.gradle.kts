53a2e0adc9608311826fb532c06eb69dd38e35b7
Ihor Komisarenko <bulletss666@gmail.com>
2023-03-02 15:34:52 +0200

    2: Configure and Register Tasks: 8: Configure Tasks added by Plugins

--- gradle/plugins/java-plugins/src/main/kotlin/my-java-base.gradle.kts
+++ gradle/plugins/java-plugins/src/main/kotlin/my-java-base.gradle.kts
@@ -5,3 +5,23 @@ plugins {
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(11))
 }
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+}
+
+//tasks.compileJava {
+//    options.encoding = "UTF-8"
+//}
+//
+//tasks.compileTestJava {
+//    options.encoding = "UTF-8"
+//}
+
+tasks.test {
+
+}
+
+tasks.javadoc {
+
+}
